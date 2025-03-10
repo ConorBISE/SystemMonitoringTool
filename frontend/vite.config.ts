e6e647cf100328acfdad0b573b8e88a39c2a6c39
ConorBobbleHat <conorb@firstpartners.net>
2025-03-10 15:06:38 +0000

    Vite allowed hosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/ 
 export default defineConfig({
   plugins: [
     TanStackRouterVite({
@@ -11,4 +11,7 @@ export default defineConfig({
     }),
     react(),
   ],
+  server: {
+    allowedHosts: ["systemmonitortool.ddns.net"]
+  }
 })
