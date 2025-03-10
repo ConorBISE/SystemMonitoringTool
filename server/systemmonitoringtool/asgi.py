26a32bb502f7ba2d66f7b6c53882d27c76dd02a7
ConorBobbleHat <conorb@firstpartners.net>
2025-03-10 12:58:53 +0000

    ASGI fix

--- server/systemmonitoringtool/asgi.py
+++ server/systemmonitoringtool/asgi.py
@@ -12,7 +12,7 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "systemmonitoringtool.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.systemmonitoringtool.settings")
 
 django_asgi_application = get_asgi_application()
 
